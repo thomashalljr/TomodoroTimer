a26a2994772060a4b8800e0a9696403048cf054c
Thomas Hall Jr <thomashalljunior@gmail.com>
2020-01-09 08:38:16 -0500

    Getting app ready for Heroku

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class TomodoroTimer extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeCountingDown: this.props.initialTime * 60 * 1000,
+      running: false,
+      customTimer: null
+    };
+
+    this.handleCustomTimeChange = this.handleCustomTimeChange.bind(this);
+  }
+
+  handleCustomTimeChange(event) {
+    // Only allow input up to two numbers
+    event.target.value = event.target.value.slice(0,event.target.maxLength);
+    this.setState({customTimer: event.target.value});
+  }
+
+  componentDidMount() {
+    var self = this;
+    document.title = "Tomodoro Timer";
+
+    document.getElementById("updateButton").addEventListener("click", function(evt) {
+      evt.preventDefault();
+      var initialTime = self.state.customTimer;
+
+      if (initialTime != "" && initialTime > 0) {
+        self.setState({ timeCountingDown: initialTime * 60 * 1000 });
+      } else if (initialTime < 1) {
+        alert("Please enter time of 1 or more minutes.");
+      }
+    });
+
+    document.getElementById("playPauseButton").addEventListener("click", function(evt) {
+      evt.preventDefault();
+      self.setPlayPauseButtonText();
+
+      if (!self.state.running) {
+        self.interval = setInterval(self.elapseTime.bind(self), 1000);
+        self.setState({ running: true });
+      } else {
+        clearInterval(self.interval);
+        self.setState({ running: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  elapseTime() {
+    this.setState({ timeCountingDown: this.minusTime() });
+
+    if (this.state.timeCountingDown == 0) {
+      alert("Break time!");
+      document.getElementById("alarm").play();
+      clearInterval(this.interval);
+      this.setPlayPauseButtonText();
+
+      var initialTime = this.state.customTimer;
+
+      if (initialTime != "") {
+        this.setState({ timeCountingDown: initialTime * 60 * 1000 });
+      } else {
+        this.setState({ timeCountingDown: this.props.initialTime * 60 * 1000 });
+      }
+
+      if (!this.state.running) {
+        this.setState({ running: true });
+      } else {
+        this.setState({ running: false });
+      }
+    }
+  }
+
+  minusTime() {
+    return this.state.timeCountingDown - 1000;
+  }
+
+  setPlayPauseButtonText() {
+    var playPauseButton = document.getElementById("playPauseButton");
+    var playPauseButtonText = playPauseButton.textContent;
+
+    if (playPauseButtonText == "Play") {
+      playPauseButton.textContent = "Pause";
+    } else {
+      playPauseButton.textContent = "Play";
+    }
+  }
+
+  render() {
+    return (
+      <div id="container">
+        <form name="customTimer">
+          <div><label>How many minutes should timer be?</label></div>
+          <div id="updateAndPlay">
+            <input type="number" id="initialTime" min="1" maxLength="2" value={this.state.value} onChange={this.handleCustomTimeChange} />
+            <button id="updateButton">Update</button>
+            <button id="playPauseButton">Play</button>
+          </div>
+        </form>
+        <div id="timeElapsed">{moment.utc(this.state.timeCountingDown).format("mm:ss")}</div>
+        <audio id="alarm" src="./audio/alarm.mp3" preload="auto" type="audio/mpeg" />
+      </div>
+    );
+  }
+}
+
+export default () => (
+  <div className="app">
+    { React.createElement(TomodoroTimer, { initialTime: 0.1, restingTime: 1 }) }
+  </div>
+);
